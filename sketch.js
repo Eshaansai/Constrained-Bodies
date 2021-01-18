1774e5e6db411dcdc89941aa5d2ae8ce8b82df7d
Eshaansai <74912508+Eshaansai@users.noreply.github.com>
2021-01-18 18:55:31 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,9 +1,19 @@
 const Engine = Matter.Engine;
 const World= Matter.World;
 const Bodies = Matter.Bodies;
+const Constraint= Matter.Constraint;
+
 
 var engine, world;
 var box1, pig1;
+var backgroundImg;
+var platform;
+var log6;
+var chain;
+
+function preload() {
+    backgroundImg = loadImage("sprites/bg.png");
+}
 
 function setup(){
     var canvas = createCanvas(1200,400);
@@ -12,7 +22,7 @@ function setup(){
 
     
     ground = new Ground(600,height,1200,20)
-
+    platform=new Ground(150,305,300,170);
     box1 = new Box(700,320,70,70);
     box2 = new Box(920,320,70,70);
     pig1 = new Pig(810, 350);
@@ -29,11 +39,13 @@ function setup(){
     log5 = new Log(870,120,150, -PI/7);
 
     bird = new Bird(100,100);
+    log6 = new Log(100,100,100, PI/2);
 
+  chain=new Chain (bird.body,log6.body);
 }
 
 function draw(){
-    background(0);
+    background(backgroundImg);
     Engine.update(engine);
     console.log(box2.body.position.x);
     console.log(box2.body.position.y);
@@ -54,4 +66,8 @@ function draw(){
     log5.display();
 
     bird.display();
+    log6.display();
+    platform.display();
+
+    chain.display();
 }
